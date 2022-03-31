782ebc7695377b0ddbb0e5ed9778e33afb0b2b29
Antonio <adr343@gmail.com>
2022-03-31 17:16:01 +0200

    commit

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -1,15 +1,18 @@
 package academy.digitallab.store.shopping.client;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import academy.digitallab.store.shopping.LoadBalancerConfiguration;
 import academy.digitallab.store.shopping.model.Customer;
 
 @FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
+@LoadBalancerClient(name = "customer-service", configuration = LoadBalancerConfiguration.class)
 public interface CustomerClient {
 
-    @GetMapping(value = "/customers/{id}")
-    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
+	@GetMapping(value = "/customers/{id}")
+	public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
 }
--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/ProductClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/ProductClient.java
@@ -1,19 +1,23 @@
 package academy.digitallab.store.shopping.client;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import academy.digitallab.store.shopping.LoadBalancerConfiguration;
 import academy.digitallab.store.shopping.model.Product;
 
 @FeignClient(name = "product-service")
+@LoadBalancerClient(name = "product-service", configuration = LoadBalancerConfiguration.class)
 public interface ProductClient {
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
+	@GetMapping(value = "/products/{id}")
+	public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
 
-    @GetMapping(value = "/{id}/stock")
-    public ResponseEntity<Product> updateStockProduct(@PathVariable  Long id ,@RequestParam(name = "quantity", required = true) Double quantity);
-    }
+	@GetMapping(value = "/products/{id}/stock")
+	public ResponseEntity<Product> updateStockProduct(@PathVariable Long id,
+			@RequestParam(name = "quantity", required = true) Double quantity);
+}
